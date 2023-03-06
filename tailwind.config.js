621ed459238856ac3ede95bb6f7e36db5e7f6000
willemgovaerts <willem.govaerts00@gmail.com>
2023-03-06 22:22:06 +0100

    style changes to about

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}', './node_modules/flowbite/**/*.js'],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend:
-    {
+    extend: {
       fontFamily: {
-        'opensans': ['Open Sans', 'sans-serif'],
-        'raleway': ['Raleway', 'sans-serif'],
-        'holtwood': ['Holtwood One SC', 'serif']
+        opensans: ["Open Sans", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+        holtwood: ["Holtwood One SC", "serif"],
       },
 
       animation: {
-        fadeIn: "fadeIn 1s ease-in forwards"
+        fadeIn: "fadeIn 1s ease-in forwards",
       },
       keyframes: {
         fadeIn: {
           "0%": { opacity: 0 },
-          "100%": { opacity: 1 }
-        }
-      }
-    }
+          "100%": { opacity: 1 },
+        },
+      },
+    },
+    container: {
+      center: true,
+    },
+    boxShadow: {
+      DEFAULT: "9px 9px 30px -11px rgba(0,0,0,0.75)",
+      sm: "0px 6px 15px -5px rgba(0, 0, 0, 0.71)",
+    },
   },
 
-  plugins: [
-    require('flowbite/plugin')
-  ]
-}
+  plugins: [require("flowbite/plugin")],
+};

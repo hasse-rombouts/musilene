c45bf3c0aa135d75adbc960ed88fd6fabb2f37b1
hasse-rombouts <hasse.rombouts@gmail.com>
2023-04-21 14:05:54 +0200

    fix for build.py

--- rendering/build.py
+++ rendering/build.py
@@ -8,15 +8,16 @@ from typing import Dict
 from render import format_file
 
 def index_files():
-    folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..\\build'))
+    path_one_level_up = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    folder_path = os.path.join(path_one_level_up, 'build')
     shutil.rmtree(folder_path, ignore_errors=True)
     files = []
-    src_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..\\src'))
+    src_folder = os.path.join(path_one_level_up, 'src')
     for dirpath, _, filenames in os.walk(src_folder):
         for filename in filenames:
             if not filename.endswith('.html'):
                 src_path = os.path.join(dirpath, filename)
-                dest_path =src_path.replace('\\src\\', '\\build\\')
+                dest_path = src_path.replace('\\src\\', '\\build\\').replace('/src/', '/build/')
                 os.makedirs(os.path.dirname(dest_path), exist_ok=True)
                 shutil.copy2(src_path, dest_path)
             else: 
@@ -28,7 +29,7 @@ def process_file(filename: str, data: Dict):
         return
     if not filename.endswith('.html'):
         print("Copying file", filename)
-        dest_path =filename.replace('\\src\\', '\\build\\')
+        dest_path =filename.replace('\\src\\', '\\build\\').replace('/src/', '/build/')
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
         shutil.copy2(filename, dest_path)
         return
@@ -37,13 +38,13 @@ def process_file(filename: str, data: Dict):
         content = f.read()
         result = format_file(content, data)
         
-        build_file = filename.replace('\\src\\', '\\build\\')
+        build_file = filename.replace('\\src\\', '\\build\\').replace('/src/', '/build/')
         os.makedirs(os.path.dirname(build_file), exist_ok=True)
         with open(build_file, 'w') as f:
             f.write(result)
 
 def get_data():
-    filename = os.path.abspath(os.path.join(os.path.dirname(__file__), '..\\content\\data.json'))
+    filename = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'content', 'data.json'))
     with open(filename, 'r') as f:
         return json.load(f)
 

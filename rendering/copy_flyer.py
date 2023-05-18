82b55e86547fd1c5249e142350e31aa074de954d
willemgovaerts <willem.govaerts00@gmail.com>
2023-05-18 18:09:55 +0200

    change flyer

--- rendering/copy_flyer.py
+++ rendering/copy_flyer.py
@@ -1,10 +1,11 @@
 import os
 from PIL import Image
 
+
 def copy_flyer():
-    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     flyer_path = get_flyer_path(base_path)
-    dest_path = os.path.join(base_path, 'build', 'assets', 'figures', 'flyer.webp')
+    dest_path = os.path.join(base_path, "build", "assets", "figures", "flyer.webp")
     with Image.open(flyer_path) as image:
         # Calculate the height based on the aspect ratio.
         height = round(800 / (image.width / image.height))
@@ -13,10 +14,11 @@ def copy_flyer():
         # Convert the image to WebP format and save it.
         resized_image.save(dest_path, format="webp")
 
+
 def get_flyer_path(base_path):
-    extensions = ['png', 'jpg', 'jpeg', 'webp']
+    extensions = ["png", "jpg", "jpeg", "webp"]
     for extension in extensions:
-        flyer_path = os.path.join(base_path, 'src', 'flyer.' + extension)
+        flyer_path = os.path.join(base_path, "src", "flyer." + extension)
         if os.path.exists(flyer_path):
             return flyer_path
     raise ValueError("No flyer found in the src folder.")

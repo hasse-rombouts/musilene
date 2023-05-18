6968d4eeaecc9e02d1918aa88d45bb73a1dde3f4
willemgovaerts <willem.govaerts00@gmail.com>
2023-05-18 18:18:52 +0200

    correctly set index link

--- rendering/remove_file_extension.py
+++ rendering/remove_file_extension.py
@@ -2,5 +2,5 @@ import re
 
 
 def remove_file_extensions(content: str):
-    content = re.sub(r'href="index.html"', r'href="index.html"', content)
+    content = re.sub(r'href="index.html"', r'href="./"', content)
     return re.sub(r'href="([^"]+).html"', r'href="\1"', content)
